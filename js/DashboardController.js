42c29039cc38e74e8f584f7f9af8fc312f76cd0e
Adam Vigneaux <adamvigneaux@gmail.com>
2015-03-09 20:11:23 -0400

    Add DashboardController

---
+++ js/DashboardController.js
@@ -0,0 +1,23 @@
+app.controller('DashboardController', ['$filter', 'DatabaseFactory', 'DataService', function ($filter, DatabaseFactory, DataService) {
+
+  var dashboard = this;
+
+  // Get current banner
+  DatabaseFactory.get('message').then(function (response) {
+    dashboard.banner = response.data;
+  });
+
+  // Get all users
+  DatabaseFactory.getAll().then(function (response) {
+
+    dashboard.allUsers = DataService.extractDocs(response);
+    dashboard.allUsers = DataService.cleanUsers(dashboard.allUsers);
+    dashboard.totalUsers = dashboard.allUsers.length;
+    dashboard.users = {
+      'recent': $filter('orderBy')(dashboard.allUsers, 'lastLogin', true),
+      'frequent': $filter('orderBy')(dashboard.allUsers, 'totalLogins', true),
+      'new': $filter('orderBy')(dashboard.allUsers, 'firstLogin', true)
+    };
+  });
+
+}]);
