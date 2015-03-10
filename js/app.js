c0869e1ad375449f353f73944e59c58c1d67a5e6
Adam Vigneaux <adamvigneaux@gmail.com>
2015-03-09 20:11:12 -0400

    Add app definition and database constant

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var app = angular.module('gocostudent-dashboard', []);
+
+app.constant('DatabaseConstant', {
+  'url': 'https://adamvig.cloudant.com',
+  'username': 'ressiblythenahightedgers',
+  'password': 'W4LnuQ6uJpkioDRk5Pnvvoms'
+});
