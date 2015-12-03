486073a68d17279cf867718872c32cfc2cbff5e9
Adam Vigneaux <adamvigneaux@gmail.com>
2015-12-03 09:01:28 -0500

    Add Highland controller and factory

---
+++ js/HighlandController.js
@@ -0,0 +1,3 @@
+app.controller('HighlandController', [function () {
+  var highland = this;
+}]);
---
+++ js/HighlandFactory.js
@@ -0,0 +1,6 @@
+app.factory('HighlandFactory', [function () {
+
+  var highlandFactory = this;
+
+  return highlandFactory;
+}]);
