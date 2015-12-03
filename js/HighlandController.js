31ca6d921f70f2bb3a2cd78ee62d2af294739cac
Adam Vigneaux <adamvigneaux@gmail.com>
2015-12-03 11:18:36 -0500

    Add login and loading to Highland Express

--- js/HighlandController.js
+++ js/HighlandController.js
@@ -1,3 +1,18 @@
-app.controller('HighlandController', [function () {
+app.controller('HighlandController', ['LoginService', 'HighlandFactory', function (LoginService, HighlandFactory) {
   var highland = this;
+  var pageName = "HighlandExpress";
+
+  highland.loading = true;
+
+  highland.auth = {
+    "password": "highland-staff",
+    "userAttempt": null,
+    "wrong": false,
+    "authenticated": LoginService.checkLogin(pageName)
+  };
+
+  // Log in to dashboard
+  highland.login = function () {
+    highland.auth = LoginService.login(highland.auth, pageName);
+  };
 }]);
