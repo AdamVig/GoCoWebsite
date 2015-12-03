312eb21c33c44f361899789246b57ffa430d1d2b
Adam Vigneaux <adamvigneaux@gmail.com>
2015-12-03 11:17:41 -0500

    Add support for multiple pages in LoginService

--- js/LoginService.js
+++ js/LoginService.js
@@ -1,13 +1,35 @@
 app.service('LoginService', ['$timeout', '$window', function ($timeout, $window) {
 
+  var storagePrefix = 'GoCoDashboard';
+  var storageSuffix = 'Authenticated';
+
+  /**
+   * Get name to use for storing authentication data
+   * @param  {string} pageName Name of page, can be undefined
+   * @return {string}          Name to use for storing auth data
+   */
+  function getStorageName(pageName) {
+    var separator = '.';
+    var storageName = storagePrefix + separator;
+
+    if (pageName) {
+      storageName += pageName + separator;
+    }
+
+    storageName += storageSuffix;
+
+    return storageName;
+  }
+
   /**
    * Log in
-   * @param  {object} auth Contains: password, userAttempt, wrong,
-   *                       and authenticated
-   * @return {object}      Contains: password, userAttempt, wrong,
-   *                       and authenticated
+   * @param  {object} auth     Contains: password, userAttempt, wrong,
+   *                           and authenticated
+   * @param  {string} pageName Name of page
+   * @return {object}          Contains: password, userAttempt, wrong,
+   *                           and authenticated
    */
-  this.login = function (auth) {
+  this.login = function (auth, pageName) {
     var timeout = 100;
 
     if (auth.wrong === true) {
@@ -19,7 +41,8 @@ app.service('LoginService', ['$timeout', '$window', function ($timeout, $window)
     $timeout(function () {
       if (auth.userAttempt == auth.password) {
         auth.authenticated = true;
-        $window.localStorage['GoCoDashboard.Authenticated'] = true;
+        var storageName = getStorageName(pageName);
+        $window.localStorage[storageName] = true;
       } else if (auth.userAttempt) {
         auth.wrong = true;
       }
@@ -30,9 +53,11 @@ app.service('LoginService', ['$timeout', '$window', function ($timeout, $window)
 
   /**
    * Check if logged in
+   * @param  {string} pageName Name of page
    * @return {boolean}    true if logged in, false if not
    */
-  this.checkLogin = function () {
-    return $window.localStorage['GoCoDashboard.Authenticated'] || false;
+  this.checkLogin = function (pageName) {
+    var storageName = getStorageName(pageName);
+    return $window.localStorage[storageName] || false;
   };
 }]);
