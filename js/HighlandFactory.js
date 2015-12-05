e17441344b22991d72ba80244c9f8961e28b4f26
Adam Vigneaux <adamvigneaux@gmail.com>
2015-12-04 23:10:15 -0500

    Add data getting capabilities

--- js/HighlandFactory.js
+++ js/HighlandFactory.js
@@ -1,6 +1,35 @@
-app.factory('HighlandFactory', [function () {
+app.factory('HighlandFactory', ['DatabaseFactory', 'BackendService', function (DatabaseFactory, BackendService) {
 
   var highlandFactory = this;
+  var endpoint = 'highlandexpress';
+
+  highlandFactory.data = {};
+
+  /**
+   * Handle data response from database
+   * @param  {object} response Highland Express doc from database
+   */
+  function handleResponse(response) {
+    highlandFactory.data = response.data;
+  }
+
+  /**
+   * Handle error in database connection
+   * @param  {string} error Error message from database
+   */
+  function handleError(error) {
+    console.log("Error getting Highland Express data: " + error);
+  }
+
+  /**
+   * Get Highland Express data
+   * @return {object} Highland Express data
+   */
+  highlandFactory.getData = function () {
+    return BackendService.post(endpoint)
+            .success(handleResponse)
+            .error(handleError);
+  };
 
   return highlandFactory;
 }]);
